33a8200a47283511f8b0519c2d2569ccad64055b
radityanatha <radityanatha20@gmail.com>
2025-06-19 22:18:07 +0800

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/Home'
 import MedicineDetail from './pages/MedicineDetail'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import ProtectedRoute from './components/ProtectedRoute'
 
 function AppContent() {
   const location = useLocation()
@@ -16,14 +14,7 @@ function AppContent() {
       {!isLoginPage && <Header />}
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/medicine/:id" element={<MedicineDetail />} />
       </Routes>
       {!isLoginPage && <Footer />}
